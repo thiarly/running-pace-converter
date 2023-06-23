e2b94d258cb26bdeb77d96e972a54b8e45aba172
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-06-23 18:06:15 -0300

    Update router.py

--- runnerpacer/router.py
+++ runnerpacer/router.py
@@ -4,9 +4,13 @@ from runnerpacer import app
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        time = float(request.form.get('time'))
+        hour = int(request.form.get('hour'))
+        minute = int(request.form.get('minute'))
+        second = int(request.form.get('second'))
         distance = float(request.form.get('distance'))
         activity = request.form.get('activity')
+
+        time = hour * 3600 + minute * 60 + second  # Converter o tempo total em segundos
 
         if activity == 'corrida':
             pace_km, pace_mile = convert_pace(time, distance)
@@ -22,26 +26,44 @@ def index():
 
     return render_template('index.html')
 
+
+
+
 def convert_pace(time, distance):
-    pace_km = time / distance
-    pace_mile = time / (distance / 1.60934)
+    time_minutes = time // 60  # Obter a parte inteira dos minutos
+    time_seconds = time % 60  # Obter os segundos restantes
 
-    pace_km_min, pace_km_sec = divmod(pace_km, 1)
-    pace_mile_min, pace_mile_sec = divmod(pace_mile, 1)
+    total_minutes = time_minutes + time_seconds / 60  # Tempo total em minutos
 
-    pace_km_formatted = f'{int(pace_km_min)}:{float(pace_km_sec*60)}'
-    pace_mile_formatted = f'{int(pace_mile_min)}:{float(pace_mile_sec*60)}'
-    
+    pace_km = total_minutes / distance
+    pace_mile = total_minutes / (distance / 1.60934)
+
+    pace_km_minutes, pace_km_seconds = divmod(int(pace_km * 60), 60)
+    pace_mile_minutes, pace_mile_seconds = divmod(int(pace_mile * 60), 60)
+
+    pace_km_formatted = f'{pace_km_minutes:02d}:{pace_km_seconds:02d} /km'
+    pace_mile_formatted = f'{pace_mile_minutes:02d}:{pace_mile_seconds:02d} /mile'
+
     return pace_km_formatted, pace_mile_formatted
 
+
+def convert_pace_mile(time, distance):
+    pace_mile = time / (distance / 1.60934)
+
+    pace_mile_minutes, pace_mile_seconds = divmod(int(pace_mile * 60), 60)
+
+    pace_mile_formatted = f'{pace_mile_minutes:02d}:{pace_mile_seconds:02d} /mile'
+
+    return pace_mile_formatted
+
+
 def calc_average_speed_bike(time, distance):
-    avg_speed = distance / (time / 60)
-    return f'{avg_speed:.2f} km/h'
+    avg_speed = distance / (time / 3600) # Converter o tempo de segundos para horas
+    return f'{avg_speed:.2f}'
 
 def calc_swim_pace(time, distance):
     pace_100m = (time / distance) * 100
-    pace_100m_min, pace_100m_sec = divmod(pace_100m, 1)
+    pace_100m_min, pace_100m_sec = divmod(pace_100m, 60)  # Converter para minutos e segundos
 
-    pace_100m_formatted = f'{int(pace_100m_min)}:{pace_100m_sec*60:.2f}'
+    pace_100m_formatted = f'{int(pace_100m_min)}:{pace_100m_sec:02d}'  # Formato MM:SS
     return pace_100m_formatted
-
