bcd77d878073c28817b4cc64ebdd4f662d5ab4a0
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2024-05-22 16:26:45 -0300

    Update script.js

--- conversor/static/js/script.js
+++ conversor/static/js/script.js
@@ -1,10 +1,9 @@
+// @ts-nocheck
 const btNav = document.querySelector('#btNav');
 const divNav = document.querySelector('#divNav');
 
 // Alternar visibilidade do menu principal em dispositivos móveis
-// @ts-ignore
 btNav.addEventListener('click', function() {
-    // @ts-ignore
     divNav.classList.toggle('hidden');
 });
 
@@ -12,6 +11,17 @@ btNav.addEventListener('click', function() {
 function toggleDropdown(dropdownId) {
     const dropdownMenu = document.querySelector(dropdownId);
     dropdownMenu.classList.toggle('hidden');
+}
+
+// Função para fechar dropdowns se clicar fora
+function closeDropdowns(event) {
+    dropdownLinks.forEach(link => {
+        const dropdownMenu = document.querySelector(link.menuId);
+        if (!dropdownMenu.contains(event.target) && !document.querySelector(link.buttonId).contains(event.target) && !dropdownMenu.classList.contains('hidden')) {
+            // @ts-ignore
+            dropdownMenu.classList.add('hidden');
+        }
+    });
 }
 
 // Event listeners para botões de dropdown
@@ -23,6 +33,8 @@ const dropdownLinks = [
 
 dropdownLinks.forEach(link => {
     const button = document.querySelector(link.buttonId);
-    // @ts-ignore
     button.addEventListener('click', () => toggleDropdown(link.menuId));
 });
+
+// Adiciona o listener ao documento para fechar os dropdowns ao clicar fora
+document.addEventListener('click', closeDropdowns);
