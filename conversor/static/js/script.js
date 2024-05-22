e5c3b1b5e92b19e823724a03d4d4cd75b4383bf4
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2024-05-22 15:32:10 -0300

    Update script.js

--- conversor/static/js/script.js
+++ conversor/static/js/script.js
@@ -1,9 +1,28 @@
 const btNav = document.querySelector('#btNav');
 const divNav = document.querySelector('#divNav');
 
+// Alternar visibilidade do menu principal em dispositivos móveis
 // @ts-ignore
 btNav.addEventListener('click', function() {
     // @ts-ignore
     divNav.classList.toggle('hidden');
-})
+});
 
+// Função para alternar a visibilidade do dropdown
+function toggleDropdown(dropdownId) {
+    const dropdownMenu = document.querySelector(dropdownId);
+    dropdownMenu.classList.toggle('hidden');
+}
+
+// Event listeners para botões de dropdown
+const dropdownLinks = [
+    { buttonId: '#dropdownNavbarLink1', menuId: '#dropdownNavbar1' },
+    { buttonId: '#dropdownNavbarLink2', menuId: '#dropdownNavbar2' },
+    { buttonId: '#dropdownNavbarLink3', menuId: '#dropdownNavbar3' }
+];
+
+dropdownLinks.forEach(link => {
+    const button = document.querySelector(link.buttonId);
+    // @ts-ignore
+    button.addEventListener('click', () => toggleDropdown(link.menuId));
+});
