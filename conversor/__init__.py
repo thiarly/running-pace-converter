5177693958d2665adfc2465baf180ff33317881f
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2025-04-24 14:38:16 -0300

    Update __init__.py

--- conversor/__init__.py
+++ conversor/__init__.py
@@ -1,44 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-import os
-import sqlalchemy
-import time
 from flask_migrate import Migrate
+import os
 
-# Inicializa o app
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dd64ab2f40726b31b2dc390c9e6a0310'
+db = SQLAlchemy()
+migrate = Migrate()
 
-project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-database_path = os.path.join(project_dir, 'instance', 'database.db')
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'dd64ab2f40726b31b2dc390c9e6a0310'
 
+    db_url = os.getenv('DATABASE_URL')
+    if db_url and db_url.startswith('postgres://'):
+        db_url = db_url.replace('postgres://', 'postgresql://')
 
-if os.getenv('DATABASE_URL'):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + database_path
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url or 'sqlite:///instance/database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Inicializa extensões
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-# Importa modelos e rotas (garante registro no SQLAlchemy)
-from conversor import models
-from conversor import router
+    from conversor import router, models  # importa views e models
+    app.register_blueprint(router)
 
-
-
-def create_database_if_not_exists():
-    engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    inspector = sqlalchemy.inspect(engine)
-    if not inspector.has_table('suplementos'):
-        with app.app_context():
-            db.create_all()
-            print("Base de dados criada com sucesso")
-    else:
-        print("Base de dados já existe")
-
-create_database_if_not_exists()
-        
+    return app
