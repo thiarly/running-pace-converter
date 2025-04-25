2e5c61a18c6e38a2433ad32ec3ee01062cf96ded
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2025-04-25 16:44:14 -0300

    Update models.py
    criado modelo planejamentoItem

--- conversor/models.py
+++ conversor/models.py
@@ -41,3 +41,45 @@ class Suplemento(database.Model):
 
     def __repr__(self):
         return f'<Suplemento {self.nome}>'
+
+
+
+class PlanejamentoItem(database.Model):
+    __tablename__ = 'planejamento_itens'
+    id = database.Column(database.Integer, primary_key=True)
+    suplemento_id = database.Column(database.Integer, database.ForeignKey('suplementos.id'), nullable=False)
+    quantidade = database.Column(database.Integer, nullable=False)
+
+    suplemento = database.relationship('Suplemento')
+
+    def calcular_totais(self):
+        def multi(valor):
+            return round((valor or 0) * self.quantidade, 2)
+
+        return {
+            'carbo': multi(self.suplemento.carbo),
+            'sodio': multi(self.suplemento.sodio),
+            'magnesio': multi(self.suplemento.magnesio),
+            'potassio': multi(self.suplemento.potassio),
+            'calcio': multi(self.suplemento.calcio),
+            'cafeina': multi(self.suplemento.cafeina),
+            'taurina': multi(self.suplemento.taurina),
+            'beta_alanina': multi(self.suplemento.beta_alanina),
+            'citrulina': multi(self.suplemento.citrulina),
+            'creatina': multi(self.suplemento.creatina),
+            'coq10': multi(self.suplemento.coq10),
+            'carnitina': multi(self.suplemento.carnitina),
+            'leucina': multi(self.suplemento.leucina),
+            'isoleucina': multi(self.suplemento.isoleucina),
+            'valina': multi(self.suplemento.valina),
+            'arginina': multi(self.suplemento.arginina),
+            'vit_b1': multi(self.suplemento.vit_b1),
+            'vit_b2': multi(self.suplemento.vit_b2),
+            'vit_b3': multi(self.suplemento.vit_b3),
+            'vit_b6': multi(self.suplemento.vit_b6),
+            'vit_b7': multi(self.suplemento.vit_b7),
+            'vit_b9': multi(self.suplemento.vit_b9),
+            'vit_b12': multi(self.suplemento.vit_b12),
+            'vit_c': multi(self.suplemento.vit_c),
+        }
+
